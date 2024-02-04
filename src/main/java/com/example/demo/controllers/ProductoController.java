53ad7f35e1240949d1e51ac686b89d1396b5ed50
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2024-02-04 14:46:26 -0300

    Arreglo en ProductoController

--- src/main/java/com/example/demo/controllers/ProductoController.java
+++ src/main/java/com/example/demo/controllers/ProductoController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Producto;
-import com.example.demo.models.Venta;
+//import com.example.demo.models.Venta;
 import com.example.demo.repository.ProductoRepository;
-import com.example.demo.repository.VentaRepository;
+//import com.example.demo.repository.VentaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,9 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.transaction.annotation.Transactional;
 
 @RestController
 @RequestMapping("productos")
@@ -22,8 +19,8 @@ public class ProductoController {
   @Autowired
   private ProductoRepository productoRepo;
 
-  @Autowired
-  private VentaRepository ventaRepo;
+  // @Autowired
+  // private VentaRepository ventaRepo;
 
   @GetMapping
   public List<Producto> getProductos() {
@@ -43,33 +40,9 @@ public class ProductoController {
       Producto updateProducto = productoRepo.getOne(id);
       updateProducto.setNombre(producto.getNombre());
       updateProducto.setStock(producto.getStock());
-      productoRepo.save(updateProducto);
-      return ResponseEntity.ok("Producto modificado");
-    } else {
-      return ResponseEntity.status(HttpStatus.CONFLICT).body("Producto no encontrado");
-    }
-  }
-
-  // Método para actualizar precio (solo si el producto no ha sido vendido)
-  @PutMapping("modificarPrecio/{id}")
-  @Transactional
-  public ResponseEntity<String> modificarPrecio(@PathVariable Long id, @RequestBody Producto producto) {
-    Optional<Producto> optionalProducto = productoRepo.findById(id);
-
-    if (optionalProducto.isPresent()) {
-      Producto updateProducto = optionalProducto.get();
-
-      // Verificar si el producto ha sido vendido
-      boolean productoVendido = ventaRepo.existsByProductosContains(updateProducto);
-
-      if (productoVendido) {
-        return ResponseEntity.badRequest().body("No se puede actualizar el precio de un producto vendido");
-      }
-
-      // Actualizar precio
       updateProducto.setPrecio(producto.getPrecio());
       productoRepo.save(updateProducto);
-      return ResponseEntity.ok("Precio modificado");
+      return ResponseEntity.ok("Producto modificado");
     } else {
       return ResponseEntity.status(HttpStatus.CONFLICT).body("Producto no encontrado");
     }
